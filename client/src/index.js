b1b83f6d06b8cb956561141118dd726a351d5501
dropbeardan <dan.ess.001@gmail.com>
2018-01-22 22:32:36 +1100

    Added Material UI library.

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 import { App } from './components';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route exact path='/' component={App} />
-        </Switch>
-    </BrowserRouter>,
+    <MuiThemeProvider>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={App} />
+            </Switch>
+        </BrowserRouter>
+    </MuiThemeProvider>,
     document.getElementById('root')
 );
 registerServiceWorker();
