4d51c0626a20dec0b706297a6f95e95e0dcebfb8
dropbeardan <dan.ess.001@gmail.com>
2018-01-23 20:43:44 +1100

    Added landing background and restyled Home.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,29 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import injectSheet from 'react-jss';
 
 import styles from './Home.styles.js';
 
-import { LinkButton } from '../Buttons';
+import { RoundButton } from '../Buttons';
 
-import Mascot from '../../assets/images/mascot.png'
+import Mascot from '../../assets/images/mascot.png';
 
 const Home = (props) => {
 	return (
 		<section className={props.classes.Home}>
 			<div className={props.classes.Home__Container}>
 				<div className={props.classes.Home__Hero}>
-					<div>
-						<img src={Mascot} alt='Professor Oak welcomes you!' />
-					</div>
 					<div className={props.classes.Home__Hero__Description}>
 						<header className={props.classes.Home__Hero__Header}>Pokémon GraphQL</header>
 						<main className={props.classes.Home__Hero__Main}>For lovers of Pokémon</main>
-						<LinkButton
-							label="Visit the Laboratory"
-							url='/pokedex'
-							hoverColor='#E3F2FD'
-							rippleColor='#3F51B5'
-						/>
+						<Link to='/pokedex'>
+							<RoundButton
+								label="Visit the Laboratory"
+								backgroundColor='#FFFFFF'
+								hoverColor='#FCE4EC'
+								rippleColor='#3F51B5'
+								style={{
+									RoundButton: {
+										background: 'white'
+									},
+									RoundButton__Label: {
+										fontSize: '1.25em'
+									}
+								}}
+							/>
+						</Link>
+					</div>
+					<div className={props.classes.Home__Hero__Mascot}>
+						<img src={Mascot} alt='Professor Oak welcomes you!' />
 					</div>
 				</div>
 			</div>
--- client/src/components/Home/Home.styles.js
+++ client/src/components/Home/Home.styles.js
@@ -1,54 +1,71 @@
+import background from '../../assets/images/hero-background.jpg';
+
 const styles = {
 	Home: {
 		display: 'flex',
 		justifyContent: 'center',
 		alignItems: 'center',
+		height: '100%',
 		minHeight: '100vh',
-		width: '100%'
+		width: '100%',
+		backgroundImage: `url(${background})`,
+		backgroundSize: 'cover',
+		backgroundPosition: 'bottom'
 	},
 	Home__Container: {
 		display: 'flex',
 		flexDirection: 'column',
-		height: '100vh',
+		alignItems: 'center',
+		height: '100%',
 		width: '100%',
-		maxWidth: '1200px',
+		overflowY: 'auto',
+		'& > *': {
+			maxWidth: '1200px'
+		}
 	},
 	Home__Hero: {
 		display: 'flex',
 		flexWrap: 'wrap',
+		height: '100vh',
 		width: '100%',
-		height: '75vh',
 		'& > *': {
 			padding: '20px'
 		},
-		'& > *:first-child': {
-			display: 'flex',
-			flexDirection: 'column',
-			justifyContent: 'flex-end',
-			alignItems: 'center',
-			minWidth: '20%'
-		},
-		'& > *:last-child': {
-			display: 'flex',
-			flexDirection: 'column',
-			justifyContent: 'center',
-			alignItems: 'center',
-			'& > *': {
-				margin: '10px 0'
-			}
+	},
+	Home__Hero__Mascot: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'flex-end',
+		minWidth: '20%'
+	},
+	Home__Hero__Description: {
+		flex: '1',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: '100%',
+		'& > *': {
+			margin: '10px 0'
 		}
 	},
 	Home__Hero__Header: {
 		marginBottom: '1em !important',
-		fontFamily: 'Pokemon Hollow, Roboto, sans-serif',
+		fontFamily: 'Pokemon, Roboto, sans-serif',
 		fontSize: '4em',
 		lineHeight: '1.25em',
 		letterSpacing: '10px',
 		textAlign: 'center',
-		color: 'blue'
+		color: '#AB47BC'
 	},
 	Home__Hero__Main: {
-		fontSize: '2em'
+		fontSize: '2em',
+		color: '#E91E63'
+	},
+	'@media screen and (max-width: 400px)': {
+		Home__Hero__Header: {
+			fontSize: '3.5em'
+		}
 	},
 	'@media screen and (max-width: 1000px)': {
 		Home__Hero: {
@@ -59,4 +76,4 @@ const styles = {
 	}
 };
 
-module.exports = styles;
+export default styles;
