9115a809c5e1682e95987cf1ecafe60ac0597a62
dropbeardan <dan.ess.001@gmail.com>
2018-01-23 18:29:36 +1100

    Added component mocking to Unit Test.

--- client/src/components/Home/Home.test.js
+++ client/src/components/Home/Home.test.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Home from './Home';
+import { LinkButton } from '../Buttons';
+
+jest.mock('../Buttons', () => {
+  const mockComponent = () => { return <div className='mocked' />; };
+
+  return {
+    LinkButton: mockComponent
+  };
+});
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
