a10c71f961bb08d068a1129584eff2cb69f116f0
dropbeardan <dan.ess.001@gmail.com>
2018-01-24 03:57:02 +1100

    Added images for ElementBadge.

--- client/src/components/Badges/ElementBadge.js
+++ client/src/components/Badges/ElementBadge.js
@@ -5,25 +5,44 @@ import FloatingActionButton from 'material-ui/FloatingActionButton';
 
 import styles from './ElementBadge.styles.js';
 
+import normalImg from '../../assets/images/normal-type.gif';
+import fireImg from '../../assets/images/fire-type.gif';
+import waterImg from '../../assets/images/water-type.gif';
+import electricImg from '../../assets/images/electric-type.gif';
+import grassImg from '../../assets/images/grass-type.gif';
+import iceImg from '../../assets/images/ice-type.gif';
+import fightingImg from '../../assets/images/fighting-type.gif';
+import poisonImg from '../../assets/images/poison-type.gif';
+import groundImg from '../../assets/images/ground-type.gif';
+import flyingImg from '../../assets/images/flying-type.gif';
+import psychicImg from '../../assets/images/psychic-type.gif';
+import bugImg from '../../assets/images/bug-type.gif';
+import rockImg from '../../assets/images/rock-type.gif';
+import ghostImg from '../../assets/images/ghost-type.gif';
+import dragonImg from '../../assets/images/dragon-type.gif';
+import darkImg from '../../assets/images/dark-type.gif';
+import steelImg from '../../assets/images/steel-type.gif';
+import fairyImg from '../../assets/images/fairy-type.gif';
+
 const elements = {
-    normal: { src: '' },
-    fire: { src: '' },
-    water: { src: '' },
-    electric: { src: '' },
-    grass: { src: '' },
-    ice: { src: '' },
-    fighting: { src: '' },
-    poison: { src: '' },
-    ground: { src: '' },
-    flying: { src: '' },
-    psychic: { src: '' },
-    bug: { src: '' },
-    rock: { src: '' },
-    ghost: { src: '' },
-    dragon: { src: '' },
-    dark: { src: '' },
-    steel: { src: '' },
-    fairy: { src: '' },
+    normal: normalImg,
+    fire: fireImg,
+    water: waterImg,
+    electric: electricImg,
+    grass: grassImg,
+    ice: iceImg,
+    fighting: fightingImg,
+    poison: poisonImg,
+    ground: groundImg,
+    flying: flyingImg,
+    psychic: psychicImg,
+    bug: bugImg,
+    rock: rockImg,
+    ghost: ghostImg,
+    dragon: dragonImg,
+    dark: darkImg,
+    steel: steelImg,
+    fairy: fairyImg
 };
 
 const ElementBadge = class extends React.Component {
@@ -36,12 +55,15 @@ const ElementBadge = class extends React.Component {
                 <div className={this.props.classes.ElementBadge__Badge}>
                     <img
                         src={this.props.type && elements[this.props.type] ?
-                            elements[this.props.type].src : 'http://via.placeholder.com/200x200'}
+                            elements[this.props.type] : 'http://via.placeholder.com/200x200'}
                         alt='Unknown Type'
                     />
                 </div>
 
-                <div className={`${this.props.classes.ElementBadge__Hint} ${this.props.position ? this.props.position : ''}`}>
+                <div className={`
+                    ${this.props.classes.ElementBadge__Hint}
+                    ${this.props.position ? this.props.position : ''}
+                `}>
                     <div className='title'>{this.props.type ? this.props.type : 'Unknown'}</div>
                 </div>
             </div>
