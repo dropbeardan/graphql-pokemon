9720d9a21e961be13bf8a910326642af54ebed86
dropbeardan <dan.ess.001@gmail.com>
2018-01-23 18:38:52 +1100

    Added modules to support Link component testing.

--- client/src/components/Buttons/LinkButton.test.js
+++ client/src/components/Buttons/LinkButton.test.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { compose } from 'recompose';
+
+import { withMuiThemeProvider } from '../../providers';
+
+const withProviders = compose(
+    withMuiThemeProvider
+);
 
 import LinkButton from './LinkButton';
 
 test('renders without crashing', () => {
     const div = document.createElement('div');
 
+    const LinkButtonContainer = () => {
+        return <LinkButton url='/' label='test' />;
+    };
+
     ReactDOM.render(
-        <BrowserRouter>
-            <Route component={<LinkButton url='/' label='test' />} />
-        </BrowserRouter>
-        , div
+        withProviders(
+            <BrowserRouter>
+                <Route component={LinkButtonContainer} />
+            </BrowserRouter>
+        ), div
     );
 
     ReactDOM.unmountComponentAtNode(div);
