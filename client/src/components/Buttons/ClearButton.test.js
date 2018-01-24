ff2d7fc4ca450f09ad1efca2f624c18952677f1b
dropbeardan <dan.ess.001@gmail.com>
2018-01-25 02:16:20 +1100

    Removed unnecessary Provider statement in unit test.

--- client/src/components/Buttons/ClearButton.test.js
+++ client/src/components/Buttons/ClearButton.test.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { compose } from 'recompose';
-
-import { withMuiThemeProvider } from '../../providers';
 
 import { Output as ClearButton } from './ClearButton';
-
-const withProviders = compose(
-    withMuiThemeProvider
-);
-
-const renderComponent = (component, target) => {
-    ReactDOM.render(
-        withProviders(
-            component
-        ),
-        target
-    );
-};
 
 test('renders without crashing', () => {
     const div = document.createElement('div');
 
-    renderComponent(<ClearButton url='/' label='test' />, div);
+    ReactDOM.render(<ClearButton />, div);
 
     ReactDOM.unmountComponentAtNode(div);
 });
