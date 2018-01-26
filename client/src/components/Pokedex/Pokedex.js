c76ab3ff10ce9e3bda4e27a4f68ff6568ab314f3
dropbeardan <dan.ess.001@gmail.com>
2018-01-27 07:38:09 +1100

    Minor content change and prettier nit.

--- client/src/components/Pokedex/Pokedex.js
+++ client/src/components/Pokedex/Pokedex.js
@@ -17,135 +17,156 @@ import { SearchField } from '../InputFields';
 import { LoadingOverlay } from '../Overlays';
 
 const Pokedex = class extends React.Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            loading: true,
-            pokemons: [],
-            activePokemonId: null
-        };
+		this.state = {
+			loading: true,
+			pokemons: [],
+			activePokemonId: null
+		};
 
-        this.clearActivePokemonId = this.clearActivePokemonId.bind(this);
-        this.filterByName = this.filterByName.bind(this);
-        this.filterPokemons = this.filterPokemons.bind(this);
-        this.setActivePokemonId = this.setActivePokemonId.bind(this);
-    };
+		this.clearActivePokemonId = this.clearActivePokemonId.bind(this);
+		this.filterByName = this.filterByName.bind(this);
+		this.filterPokemons = this.filterPokemons.bind(this);
+		this.setActivePokemonId = this.setActivePokemonId.bind(this);
+	}
 
-    componentWillUpdate = (props) => {
-        if (this.state.loading && !props.PokemonGQL.loading && Array.isArray(props.PokemonGQL.pokemons)) {
-            this.setState({
-                ...this.state,
-                pokemons: props.PokemonGQL.pokemons,
-                loading: false
-            });
-        }
-    };
+	componentWillUpdate = props => {
+		if (
+			this.state.loading &&
+			!props.PokemonGQL.loading &&
+			Array.isArray(props.PokemonGQL.pokemons)
+		) {
+			this.setState({
+				...this.state,
+				pokemons: props.PokemonGQL.pokemons,
+				loading: false
+			});
+		}
+	};
 
-    clearActivePokemonId = () => {
-        this.setState({
-            ...this.state,
-            activePokemonId: null
-        });
-    };
+	clearActivePokemonId = () => {
+		this.setState({
+			...this.state,
+			activePokemonId: null
+		});
+	};
 
-    filterByName = (pokemons, name) => {
-        if (!name) {
-            return pokemons;
-        }
+	filterByName = (pokemons, name) => {
+		if (!name) {
+			return pokemons;
+		}
 
-        return pokemons.filter((pokemon) => {
-            return String(pokemon.name).toLowerCase().indexOf(String(name).toLowerCase()) >= 0;
-        });
-    };
+		return pokemons.filter(pokemon => {
+			return (
+				String(pokemon.name)
+					.toLowerCase()
+					.indexOf(String(name).toLowerCase()) >= 0
+			);
+		});
+	};
 
-    filterPokemons = (pokemons) => {
-        return (searchValue) => {
-            this.setState({
-                ...this.state,
-                pokemons: this.filterByName(pokemons, searchValue)
-            });
-        };
-    };
+	filterPokemons = pokemons => {
+		return searchValue => {
+			this.setState({
+				...this.state,
+				pokemons: this.filterByName(pokemons, searchValue)
+			});
+		};
+	};
 
-    setActivePokemonId = (id) => {
-        return () => {
-            this.setState({
-                ...this.state,
-                activePokemonId: id
-            });
-        };
-    };
+	setActivePokemonId = id => {
+		return () => {
+			this.setState({
+				...this.state,
+				activePokemonId: id
+			});
+		};
+	};
 
-    render() {
-        if (this.props.PokemonGQL && this.props.PokemonGQL.loading) {
-            return (
-                <LoadingOverlay size={120} description='Starting Pokedex' />
-            );
-        };
+	render() {
+		if (this.props.PokemonGQL && this.props.PokemonGQL.loading) {
+			return <LoadingOverlay size={120} description="Starting Pokédex" />;
+		}
 
-        const pokemons = this.props.PokemonGQL.pokemons ? this.props.PokemonGQL.pokemons : [];
+		const pokemons = this.props.PokemonGQL.pokemons
+			? this.props.PokemonGQL.pokemons
+			: [];
 
-        const detailsWindow = (
-            <div className={this.props.classes.Pokedex__Details}>
-                <Details
-                    activePokemonId={this.state.activePokemonId}
-                    onClose={this.clearActivePokemonId}
-                    changePokemonHandler={this.setActivePokemonId}
-                />
-            </div>
-        );
+		const detailsWindow = (
+			<div className={this.props.classes.Pokedex__Details}>
+				<Details
+					activePokemonId={this.state.activePokemonId}
+					onClose={this.clearActivePokemonId}
+					changePokemonHandler={this.setActivePokemonId}
+				/>
+			</div>
+		);
 
-        return (
-            <section className={this.props.classes.Pokedex} >
-                <div className={this.props.classes.Pokedex__Container}>
-                    <Toolbar className={this.props.classes.Pokedex__Filter} style={styles.Pokedex__Filter}>
-                        <ToolbarGroup>
-                            <Link to='/' title='Pokémon GraphQL Home' className={this.props.classes.Pokedex__Filter__Logo}>
-                                <Logo />
-                            </Link>
-                            <ToolbarSeparator />
-                        </ToolbarGroup>
-                        <ToolbarGroup>
-                            <SearchField floatingLabelText='Pokémon Name' onChange={this.filterPokemons(pokemons)} />
-                        </ToolbarGroup>
-                    </Toolbar>
+		return (
+			<section className={this.props.classes.Pokedex}>
+				<div className={this.props.classes.Pokedex__Container}>
+					<Toolbar
+						className={this.props.classes.Pokedex__Filter}
+						style={styles.Pokedex__Filter}
+					>
+						<ToolbarGroup>
+							<Link
+								to="/"
+								title="Pokémon GraphQL Home"
+								className={
+									this.props.classes.Pokedex__Filter__Logo
+								}
+							>
+								<Logo />
+							</Link>
+							<ToolbarSeparator />
+						</ToolbarGroup>
+						<ToolbarGroup>
+							<SearchField
+								floatingLabelText="Pokémon Name"
+								onChange={this.filterPokemons(pokemons)}
+							/>
+						</ToolbarGroup>
+					</Toolbar>
 
-                    <div className={this.props.classes.Pokedex__Results}>
-                        {this.state.pokemons.map((pokemon) => {
-                            return (
-                                <PokemonCard
-                                    key={pokemon.id}
-                                    image={pokemon.image}
-                                    number={pokemon.number}
-                                    name={pokemon.name}
-                                    types={pokemon.types}
-                                    classification={pokemon.classification}
-                                    hint='Click to View Details'
-                                    onClick={this.setActivePokemonId(pokemon.id)}
-                                />
-                            );
-                        })}
-                    </div>
-                </div>
+					<div className={this.props.classes.Pokedex__Results}>
+						{this.state.pokemons.map(pokemon => {
+							return (
+								<PokemonCard
+									key={pokemon.id}
+									image={pokemon.image}
+									number={pokemon.number}
+									name={pokemon.name}
+									types={pokemon.types}
+									classification={pokemon.classification}
+									hint="Click to View Details"
+									onClick={this.setActivePokemonId(
+										pokemon.id
+									)}
+								/>
+							);
+						})}
+					</div>
+				</div>
 
-                {this.state.activePokemonId ? detailsWindow : null}
-
-            </section >
-        );
-    };
+				{this.state.activePokemonId ? detailsWindow : null}
+			</section>
+		);
+	}
 };
 
 const Output = compose(
-    graphql(Pokemon.getAllSummary, {
-        name: 'PokemonGQL',
-        options: (props) => ({
-            variables: {
-                first: 9999
-            }
-        })
-    }),
-    injectSheet(styles)
+	graphql(Pokemon.getAllSummary, {
+		name: 'PokemonGQL',
+		options: props => ({
+			variables: {
+				first: 9999
+			}
+		})
+	}),
+	injectSheet(styles)
 )(Pokedex);
 
 export { Pokedex as Base, Output };
